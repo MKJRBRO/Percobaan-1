3765d5d37f8e155e19e08a56b94f15b54bb0a208
MKJRBRO <mukajur11@gmail.com>
2025-11-06 13:32:14 +0700

    Commit

--- SiakadFor01.java
+++ SiakadFor01.java
@@ -6,19 +6,21 @@ public class SiakadFor01 {
         int lowest = 100;
 
         for (int i = 1; i <= 10; i++) {
-            System.out.println("Enter The Grade Of The " + i + "Student:");
+            System.out.println("Enter The Grade Of Student " + i + ":");
             int grade = input.nextInt();
             if (grade > highest) {
-                if (grade < lowest) {
-                    lowest = grade;
-                }
+                highest = grade;
+            }
+            if (grade < lowest) {
+                lowest = grade;
+            }
             }
             System.out.println("The Highest Grade : " + highest);
             System.out.println("The Lowest Grade : " + lowest);
-                System.out.println("The Lowest Grade : " + lowest);
+               
             }
 
         }
 
-    }
-}
+    
+
